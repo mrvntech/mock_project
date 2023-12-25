f34fc979b37ad985c923c31741690afe234aed2b
nguyen quang phuc <mrvntech@gmail.com>
2023-12-25 11:20:35 +0700

    implement top bar

--- src/layout/user-layout/Header.tsx
+++ src/layout/user-layout/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <>
+        <div className="top-bar">
             <div className="top-bar">
                 <div className="max-w-6xl m-auto flex gap-2">
                     <Link to={"/about-us"}>About Us</Link>
@@ -12,8 +12,26 @@ export default function Header() {
                 </div>
             </div>
             <div className="main-bar">
-                <div className="max-w-6xl m-auto">main bar</div>
+
+                <div className="max-w-6xl m-auto content">
+                    <Link to={"/"} className="name">SHOP APP</Link>
+                    <div className="flex search-form justify-between">
+                        <div className="content flex gap-3">
+                            <img src="/menu-icon.svg" className="icon" />
+                            <div>Category</div>
+                            <div className="line" />
+                            <input className="field" type="text" placeholder="Search Items" />
+                        </div>
+                        <div className="action">
+                            <img src="./search-icon.svg" className="icon" />
+                        </div>
+                    </div>
+                    <div className="flex gap-4">
+                        <img src="/cart-icon.svg" />
+                        <img src="/user-icon.svg" />
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/layout/user-layout/Main.tsx
+++ src/layout/user-layout/Main.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from "react";
 
 export default function Main({ children }: { children: ReactNode }) {
     return (
-        <>
+        <div className="main-content">
             {children}
-        </>
+
+        </div>
     )
 }
